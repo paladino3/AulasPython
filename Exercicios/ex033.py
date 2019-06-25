9e5e6a7fd4034a8de4718e719446083c7300300a
Wesley Rolim <rolim.r@icloud.com>
2019-06-25 10:29:44 -0300

    ex33 feito

--- Exercicios/ex033.py
+++ Exercicios/ex033.py
@@ -6,9 +6,16 @@ A=int(input('Número 1: '))
 B=int(input('Número 2: '))
 C=int(input('Número 3: '))
 
-if (A > B & A > C & C > B):
-    print('Número:{} é maior do que {}, e também maior que {}.'.format(A,C,B))
-elif(B > A & B > C & C > A):
-    print('Número:{} é maior do que {}, e támbem maior que {}'.format(B, C, A))
-else:
+if (A >= B & B >= C):
+    print('Número:{} é maior do que {}, e também maior que {}.'.format(A,B,C))
+elif(B >= A & A >= C):
+    print('Número:{} é maior do que {}, e támbem maior que {}'.format(B,A,C))
+elif(C >= A & A >= B):
     print('Número:{} é maior do que {}, e támbem maior que {}'.format(C,A,B))
+elif(C >= B & B >= A):
+    print('Número:{} é maior do que {}, e támbem maior que {}'.format(C,B,A))
+elif(B >= C & C >= A):
+    print('Número:{} é maior do que {}, e támbem maior que {}'.format(B,C,A))
+else:
+    print('Número:{} é maior do que {}, e támbem maior que {}'.format(A,C,B))
+    print('fim')
